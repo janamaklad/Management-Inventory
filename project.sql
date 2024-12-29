0fab40593c8b49ffbcc773c48432bb1238ae27ed
janamaklad <jana2200074@miuegypt.edu.eg>
2024-12-29 19:54:51 +0200

    sql

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 23, 2024 at 12:17 PM
+-- Generation Time: Dec 25, 2024 at 12:29 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -67,7 +67,16 @@ CREATE TABLE `navbar_buttons` (
 INSERT INTO `navbar_buttons` (`ID`, `button_name`, `pagelink`, `usertype_id`) VALUES
 (1, 'Home', 'http://localhost/Management-Inventory/Homepage.php', 0),
 (2, 'About us', 'http://localhost/Management-Inventory/Aboutus.php', 0),
-(3, 'Contact us', 'http://localhost/Management-Inventory/Contact.php', 0);
+(3, 'Contact us', 'http://localhost/Management-Inventory/Contact.php', 0),
+(7, 'Log out', ' \'http://localhost/Management-Inventory/Admin/logout.php\'', 0),
+(8, 'Suppliers', '	\'http://localhost/Management-Inventory/Admin/Suppliers.php', 1),
+(9, 'Reports', 'http://localhost/Management-Inventory/Admin/report.php', 1),
+(10, 'Log Out', '	http://localhost/Management-Inventory/Admin/logout.php', 1),
+(11, 'Dashboard', '	http://localhost/Management-Inventory/Admin/Admin.php', 1),
+(12, 'Login', 'http://localhost/Management-Inventory/verify/login.php', 0),
+(13, 'Sign Up', 'http://localhost/Management-Inventory/verify/register.php', 0),
+(14, 'Reports', 'http://localhost/Management-Inventory/Admin/report.php', 2),
+(15, 'Stock Management ', '#', 2);
 
 -- --------------------------------------------------------
 
@@ -110,6 +119,40 @@ CREATE TABLE `pages` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int(11) NOT NULL,
+  `card_name` varchar(255) NOT NULL,
+  `card_number` text NOT NULL,
+  `expiry_date` varchar(7) NOT NULL,
+  `cvv` text NOT NULL,
+  `iv` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`id`, `card_name`, `card_number`, `expiry_date`, `cvv`, `iv`, `created_at`) VALUES
+(1, 'hania maher', 'H5nDVsOFF00AndvE9LGGHj0sRFPMpc8PKdf9VeEh5RE=', '2024-12', 'vdUh68DgWNJOj2QMXb4w1w==', 'bntP7utk0figfBakDHMLQg==', '2024-12-24 13:31:18'),
+(2, 'hania', 'ny1P2fqNbVUIwp6qGRqAer3lZf4p+WRMoZBqDq0fVtU=', '2024-08', '3Ng6zld4zVj7Na4CnDu6rg==', 'rbOxbSHQgSqxqu1dE/gp1A==', '2024-12-24 13:32:48'),
+(3, 'hania', '4aOP3EMrvC5RkfzU5H408DVhXHu6ZyOfPmOkzwl79rc=', '2024-08', 'ZRZ6G50UCruVaOsIv3DHdw==', 'T+zOfiqd751UOuvt7z6J9Q==', '2024-12-24 13:34:21'),
+(4, 'hania', 'P+UIXR2TjvRoafMEI7VzdOeUn103oYRJZXxz02HPiu0=', '2024-12', '+7K7NwIJefFHIVjdjdQTrw==', 'PgmJcNX7D6bZauVIroxjrA==', '2024-12-24 13:37:30'),
+(5, 'rawan', '7BH8nv9DAfInHfGauy7PDmMvJAABCB3YEWXS45U0HME=', '2024-11', 'yO4m/OnyKjP/1qrPtDGKJA==', 'j8U9yC6bKrKafvm3jIZc+w==', '2024-12-24 13:41:12'),
+(6, 'hania', '6tJoek4EBsa1gGzcyvY3iwaaTvG3sFTIxtYJcNWPXgo=', '2024-12', 'H9lsrOPJKZD7+E658LwT/w==', 'iCFqK4TlM/TSGsVwk6zPYQ==', '2024-12-24 13:44:02'),
+(7, 'sama', 'FGfgTT2oDXG0da63yTBergBOv3/c5naivUeU4qlpb4g=', '2024-12', '1v9A+KM9h/lNWiNScW2gtg==', '+SbpHg1WLO1SuFAOJw8YBA==', '2024-12-24 13:51:12'),
+(8, 'hania', 'QyC1+IVTxysEA6t+DeSSueEVT6NDsZseSLuihlz4bic=', '2024-12', '8XmI5XXSvC++5TyfeHLBfw==', 'GiHd9vxpQANEVb7BUfwkVg==', '2024-12-24 14:07:14'),
+(9, 'hania', 'mvglBS13wS6ICR/gU9PJ68kUuhHlil9DjNSU/OaikMY=', '2024-12', '3HqlnHITc2AEf0P3nVAuaQ==', 'ZY8lucHL5ogYXFVQiTYjXA==', '2024-12-24 15:38:08'),
+(10, 'hania', '0X8/nOw8QVXbjGVlLt4lia418Gj6jpn9jAk79XsZmco=', '2024-12', 'ftQdOcNAaUGQhBpKqftGnA==', 'YH0Dz282+pPB/MG0WBgdNQ==', '2024-12-24 15:38:15'),
+(11, 'hania', 'R8005liSv420zmTNpaqEINEhispQqROJoeyBUFOOdBY=', '2024-12', 'wZH0ngwgloefIMOw7urpCg==', 'cRvG2NVBlocWqWrxaBsCvQ==', '2024-12-24 15:38:18'),
+(12, 'Bahig Mahmoud', 'OJo6inZgNP/QQJ1Xu96+yQ4FUkbn9eomqzELrHU60bk=', '2024-12', 'v3z6ClDWlMV6mEHny4IptA==', 'PY3JvUif6xOOqsU2WjxLsA==', '2024-12-25 11:10:30');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -136,7 +179,8 @@ INSERT INTO `products` (`ID`, `ProductName`, `Price`, `SellerName`, `Picture`, `
 (7, 'chocolate', 20, 'Dairy Milk', '', 40, '/Management-Inventory/images/vegan-milk-chocolate-recipe.jpg', 5, 'Sweets'),
 (11, 'Redbull', 70, 'redbull', '', 35, '\\Management-Inventory\\images\\redbull.png', 0, 'Drinks'),
 (12, 'croissant', 20, 'TBS', '', 9, '/Management-Inventory/images/images.jpg', 0, 'Bakery'),
-(13, 'TOMATO', 10, 'tomato', '', 50, '\\Management-Inventory\\images\\TOMATO.jpg', 0, 'Groceries');
+(13, 'TOMATO', 10, 'tomato', '', 50, '\\Management-Inventory\\images\\TOMATO.jpg', 0, 'Groceries'),
+(14, 'hot chocolate', 50, 'TBS', '', 20, 'https://www.foodandwine.com/thmb/V1OEgtLQGUv_w2Fvm40WMLsJ4rk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Indulgent-Hot-Chocolate-FT-RECIPE0223-fd36942ef266417ab40440374fc76a15.jpg', 0, 'Drinks');
 
 -- --------------------------------------------------------
 
@@ -194,7 +238,8 @@ INSERT INTO `users` (`ID`, `Name`, `Email`, `Password`, `usertype_id`) VALUES
 (18, 'sandy', 'sandy@gmail.com', '$2y$10$Id5Oqtw4UpGPegwlaPoxl.mjtMlrcqSx/dhP5QwtEX65bELY10fHW', 1),
 (19, 'twinkies', 'twinkies2@gmail.com', '$2y$10$Wha.sz7EQaxGrTwvyXmYTekfFUQ29SF.zFKo6lYucyO2KmLqdwn5O', 2),
 (20, 'bah', 'bahigmahmoud11@gamil.com', '$2y$10$sA1oitAkvBHhdgDc1HnDmeIMxIHGmchsp.N9Lhdmi.gFNeRH6c/ui', 2),
-(21, 'todo', 'todo@gmail.com', '$2y$10$FruzkauxZE2KGbx0vu.mN.2MiIbFybTPGygWIEcq/2xgsKZQSd14K', 2);
+(21, 'todo', 'todo@gmail.com', '$2y$10$FruzkauxZE2KGbx0vu.mN.2MiIbFybTPGygWIEcq/2xgsKZQSd14K', 2),
+(22, 'Hania Maher', 'hania235@gmail.com', '$2y$10$avY7Hh2mW63Sh5FpPQG.o.nVT3OLg9gscEz8iRsxkC6V.Pzs.eoIK', 0);
 
 -- --------------------------------------------------------
 
@@ -212,7 +257,7 @@ CREATE TABLE `usertypes` (
 --
 
 INSERT INTO `usertypes` (`ID`, `Name`) VALUES
-(0, 'Customer'),
+(0, 'admin'),
 (1, 'Admin'),
 (2, 'Supplier');
 
@@ -247,6 +292,12 @@ ALTER TABLE `orders`
 --
 ALTER TABLE `pages`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `products`
@@ -287,7 +338,7 @@ ALTER TABLE `chat_messages`
 -- AUTO_INCREMENT for table `navbar_buttons`
 --
 ALTER TABLE `navbar_buttons`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -302,10 +353,16 @@ ALTER TABLE `pages`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
@@ -317,7 +374,7 @@ ALTER TABLE `suppliers`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
